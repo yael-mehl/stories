6d2488d02e1b86a4e488e35f3c37b593517b9c1f
ericyork <eyork@clarkson.edu>
2022-12-21 08:18:42 -0500

    renames player js and improves commenting

--- assets/player.js
+++ assets/player.js
@@ -1,5 +1,5 @@
+// toggles sound play when entities with sound are clicked/tapped/focused 
 var soundEls = document.querySelectorAll('[sound]')
-
     for (var i = 0; i < soundEls.length; i++) {
       var item = soundEls[i];
       item.addEventListener('click', e => {
@@ -7,23 +7,14 @@ var soundEls = document.querySelectorAll('[sound]')
         let isPlaying = el.components.sound.isPlaying
         soundEls.forEach(soundEl => {
           soundEl.components.sound.stopSound()
-        })
-        
+        })    
         if (!isPlaying)
           el.components.sound.playSound()
       })
     };
 
+// opens the info-card panel when i button is tapped
 function showInfo() {
   let infoCard = document.getElementById('info-card');
   infoCard.classList.toggle('show');
 }
-// var soundEls = document.querySelectorAll('[sound]');
-
-// for (var i = 0; i < soundEls.length; i++) {
-//   var item = soundEls[i];
-//   item.addEventListener('click', e => {
-//     var el = e.target;
-//     el.components.sound.play();
-//   })
-// }
