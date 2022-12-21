08e5b4b293c75126f15aec0f93afd36a55ce0dc8
ericyork <eyork@clarkson.edu>
2022-12-21 10:04:24 -0500

    rewrites voice card script for clarity and adds commenting

--- assets/page.js
+++ assets/page.js
@@ -1,9 +1,10 @@
-let el = document.getElementsByClassName("voice-card");
-  let i;
-  for (i = 0; i < el.length; i++) {
-    el[i].addEventListener("click", function() {
-      let content = this.childNodes[1].nextElementSibling;
-      content.classList.toggle("show");
-      this.classList.toggle("expanded");
+let voiceCards = document.getElementsByClassName("voice-card"); // selects all voiceCard elements
+  let i; // loop counter
+  for (i = 0; i < voiceCards.length; i++) { // loops through the array
+    voiceCards[i].addEventListener("click", function() { // attaches the click handler to an anonymous function
+      let tog = this.childNodes[1].classList.toggle("opened"); // adds .opened to h4s to change :after element
+      let content = this.childNodes[1].nextElementSibling; // selects voiceCard content elements
+      content.classList.toggle("show"); // toggles .show on voiceCard contents
+      this.classList.toggle("expanded"); // toggles .expanded on entire voiceCard
     });
   }
