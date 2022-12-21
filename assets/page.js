2943f65b8f7fa00e41027054e03128b96b718b53
ericyork <eyork@clarkson.edu>
2022-12-21 08:27:47 -0500

    removes a leftover line

--- assets/page.js
+++ assets/page.js
@@ -2,7 +2,6 @@ let el = document.getElementsByClassName("voice-card");
   let i;
   for (i = 0; i < el.length; i++) {
     el[i].addEventListener("click", function() {
-      let tog = this.childNodes[1].classList.toggle("opened")
       let content = this.childNodes[1].nextElementSibling;
       content.classList.toggle("show");
       this.classList.toggle("expanded");
